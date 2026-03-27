98bdc682d3820903f6125a6e06affb4bcf2de4cd
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2026-03-27 18:27:53 +0530

    feat: add main.js

--- src/main.js
+++ src/main.js
@@ -1,60 +1,89 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import OpenAI from "openai";
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src=${viteLogo} class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+console.log(import.meta.env.VITE_OPENAI_API_KEY);
+const openai = new OpenAI({
+  dangerouslyAllowBrowser: true,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+});
 
-<div class="ticks"></div>
+const translateContainerEl = document.querySelector(".translate-container");
+const translatedContainerEl = document.querySelector(".translated-container");
+const translateBtn = document.getElementById("translate-btn");
+const textareaEl = document.querySelector(".textarea");
+const frenchRadioBtn = document.getElementById("french");
+const spanishRadioBtn = document.getElementById("spanish");
+const japaneseRadioBtn = document.getElementById("japanese");
+const originalTextEl = document.getElementById("original-text");
+const translatedTextEl = document.getElementById("translated-text");
+const resetBtn = document.getElementById("reset-btn");
+const errorMessageEl = document.querySelector(".error-message");
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src=${viteLogo} alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+translateBtn?.addEventListener("click", async () => {
+  const textToTranslate = textareaEl.value;
+  if (!textToTranslate) {
+    errorMessageEl.textContent = "Please enter some text.";
+    errorMessageEl.classList.remove("hide");
+    return;
+  }
+  let selectedLanguage = "";
+  if (frenchRadioBtn.checked) {
+    selectedLanguage = frenchRadioBtn.value;
+  } else if (spanishRadioBtn.checked) {
+    selectedLanguage = spanishRadioBtn.value;
+  } else if (japaneseRadioBtn.checked) {
+    selectedLanguage = japaneseRadioBtn.value;
+  } else {
+    errorMessageEl.textContent =
+      "Please enter a language the text has to be translated into.";
+    errorMessageEl.classList.remove("hide");
+    return;
+  }
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+  errorMessageEl.classList.add("hide");
+  await translate(textToTranslate, selectedLanguage);
+});
 
-setupCounter(document.querySelector('#counter'))
+async function translate(text, targetLanguage) {
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a language expert having a grasp of many languages and can translate one language to another.",
+    },
+    {
+      role: "user",
+      content: `Translate ${text} from English to ${targetLanguage} language. Provided only the translated text in the answer, don't provide any additional content. Make any corrections in the text if the text has any errors common human errors like spelling mistakes or small grammatical errors.`,
+    },
+  ];
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: messages,
+      temperature: 0.4,
+    });
+    const translatedText = response.choices[0].message.content;
+
+    renderTranslatedText(text, translatedText);
+  } catch (error) {
+    errorMessageEl.textContent = error.message;
+    errorMessageEl.classList.remove("hide");
+    console.error("Error retrieving translated text", error);
+  }
+}
+
+function renderTranslatedText(originalText, translatedText) {
+  translateContainerEl.classList.add("hide");
+  translatedContainerEl.classList.remove("hide");
+  originalTextEl.innerText = originalText;
+  translatedTextEl.innerText = translatedText;
+}
+
+resetBtn?.addEventListener("click", () => {
+  originalTextEl.innerText = "";
+  translatedTextEl.innerText = "";
+  textareaEl.value = "";
+  frenchRadioBtn.checked = true;
+  translateContainerEl.classList.remove("hide");
+  translatedContainerEl.classList.add("hide");
+});
